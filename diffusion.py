87679d0aaf9a827127a45527baa354c6bbf4e3cc
Nicholas Rasmuson <10927951@live.mercer.edu>
2015-10-28 12:51:26 -0400

    Slightly updated, I have the cube being formed and populated with zeros

--- diffusion.py
+++ diffusion.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 import numpy as np
+import math
 
-print('Hi')
+dS = 10
+arraySize = np.power(dS,3)
+print(arraySize)
+room = np.zeros(arraySize).reshape(dS,dS,dS)
+print(room)
 
-room = np.
+
+
+
