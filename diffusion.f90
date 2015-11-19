04fc9a1e5f5b6f16ac5bc152da1fc19d9732cf8e
Nicholas Rasmuson <10927951@live.mercer.edu>
2015-11-19 14:05:23 -0500

    FInialized version bar parallelization if i figure out how.

--- diffusion.f90
+++ diffusion.f90
@@ -1,10 +1,12 @@
 program diffusion
 real room_dimension, speed_of_gas_molecules, diffusion_coefficent, timestep, distance_between_blocks, DTerm
 integer i,j,k,l,m,n,z
-integer,parameter ::  ds= 10 
+integer,parameter ::  ds= 10
+integer, parameter :: rsize = ds+2 
 real lpass, time, ratio 
 real*4 change, mval, mival
-real*4, Dimension(ds+1,ds+1,ds+1) :: room
+real*4, Dimension(rsize,rsize,rsize) :: room
+integer, Dimension(rsize,rsize,rsize) :: mask
 diffusion_coefficent = 0.175
 room_dimension = 5.0
 speed_of_gas_molecules = 250.0
@@ -13,56 +15,71 @@ distance_between_blocks = room_dimension / ds
         DTerm = diffusion_coefficent * timestep /&
 (distance_between_blocks*distance_between_blocks)
 
-        do i=1, ds
-        do j=1, ds
-        do z=1, ds
-        room(i,j,z) = 0.0
+        do i=1, rsize
+        do j=1, rsize
+        do k=1, rsize
+                room(k,j,i) = 0.0
+                mask(k,j,i) = 0
         enddo
         enddo
         enddo
 
-        print *, room
+!        print *, room
+!        print *, mask
 
-        room(1,1,1) = 1.0e21
+        do i=2, rsize-1
+        do j=2, rsize-1
+        do k=2, rsize-1
+                mask(k,j,i) = 1
+        enddo
+        enddo
+        enddo
+
+        
+        room(2,2,2) = 1.0e21
         lpass = 0
         time = 0.0
         ratio = 0.0
 
 do while( ratio< .99)
         !Go inner to outer on the loops instead of the normal outer to inner
-        do i=1,ds
-        do j=1, ds
-        do k=1, ds
-        do l=1, ds
-        do m=1, ds
-        do n=1, ds
-        if (( ( n == k ) .and.  (j ==m)   .and. ( i == l+1) ) .or.&
-                        ( ( n == k )   .and. ( j == m )   .and. ( i == l-1) ) .or.&
-                        ( ( n == k )   .and. ( j == m+1 ) .and. ( i == l)   ) .or.&
-                        ( ( n == k )   .and. ( j == m-1 ) .and. ( i == l)   ) .or.&
-                        ( ( n == k+1 ) .and. ( j == m )   .and. ( i == l)   ) .or.&
-                        ( ( n == k-1 ) .and. ( j == m )   .and. ( i == l)   ) ) THEN
-         change = &
-(room(k,j,i)-room(n,m,l))*DTerm
-        room(k,j,i) =&
-        room(k,j,i)-change
-        room(n,m,l) =&
-        room(n,m,l) + change
-        endif         
+        do i=2, rsize-1
+        do j=2, rsize-1
+        do k=2, rsize-1
+                change = (room(k,j,i)-room(k+1,j,i))*DTerm*mask(k+1,j,i)
+                room(k,j,i) = room(k,j,i)-change
+                room(k+1,j,i) = room(k+1,j,i)+change        
+                
+                change = (room(k,j,i)-room(k-1,j,i))*DTerm*mask(k-1,j,i)
+                room(k,j,i) = room(k,j,i)-change
+                room(k-1,j,i) = room(k-1,j,i)+change        
+                
+                change = (room(k,j,i)-room(k,j+1,i))*DTerm*mask(k,j+1,i)
+                room(k,j,i) = room(k,j,i)-change
+                room(k,j+1,i) = room(k,j+1,i)+change        
+                
+                change = (room(k,j,i)-room(k,j-1,i))*DTerm*mask(k,j-1,i)
+                room(k,j,i) = room(k,j,i)-change
+                room(k,j-1,i) = room(k,j-1,i)+change        
+                
+                change = (room(k,j,i)-room(k,j,i+1))*DTerm*mask(k,j,i+1)
+                room(k,j,i) = room(k,j,i)-change
+                room(k,j,i+1) = room(k,j,i+1)+change        
+                
+                change = (room(k,j,i)-room(k,j,i-1))*DTerm*mask(k,j,i-1)
+                room(k,j,i) = room(k,j,i)-change
+                room(k,j,i-1) = room(k,j,i-1)+change        
         enddo
         enddo
-        enddo
-        enddo
-        enddo
-        enddo
+        enddo 
         time = time + timestep
 
-        mval = room(1,1,1)
-mival = room(1,1,1)
+        mval = room(2,2,2)
+        mival = room(2,2,2)
 
-        do i=1, ds
-        do j=1, ds
-        do k=1, ds
+        do i=2, rsize-1
+        do j=2, rsize-1
+        do k=2, rsize-1
         mval = MAX(mval,room(k,j,i))
         mival = MIN(mival, room(k,j,i))
         enddo
@@ -70,7 +87,7 @@ mival = room(1,1,1)
         enddo
         ratio = mival/mval
 
-        print *, ratio, time
+        !print *, ratio, time
         enddo
         print *, "Box equilibrated in "
         print *, time
