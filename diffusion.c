45e92ea58f39b87ebafc410f5e4cd6445b804839
Bruno Vieira <vieira_bs@cobra.cs.mercer.edu>
2015-11-23 12:52:43 -0500

    C with memory optimization

--- diffusion.c
+++ diffusion.c
@@ -9,31 +9,32 @@ int main (int argc, char *argv[]) {
 	if(argc > 1) maxsize = atoi(argv[1]);
 	else maxsize = MAXSIZE;
 	maxsize += 2;
-	double ***cube;
-	short int ***mask;
+	double cube[maxsize][maxsize][maxsize];
+	short int maskX[maxsize], maskY[maxsize], maskZ[maxsize];
 
 	int i, j, k;
 
 	
-	cube = (double ***) malloc(maxsize * sizeof(double **));
-	mask = (short int ***) malloc((maxsize) * sizeof(short int **));
-
+//	cube = (double ***) malloc(maxsize * sizeof(double **));
+	//mask = (short int ***) malloc((maxsize) * sizeof(short int **));
+/*
 	for(i = 0; i < maxsize; i++) {
 		cube[i] = (double**) malloc(maxsize * sizeof(double*));
 		for(j = 0; j < maxsize; j++) {
 			cube[i][j] = (double*) malloc(maxsize * sizeof(double));
 		}
 	}
-
+*/
+/*
 	for(i = 0; i < maxsize; i++){
 		mask[i] = (short int**) malloc((maxsize) * sizeof(short int*));
 		for(j = 0; j < maxsize; j++){
 			mask[i][j] = (short int*) malloc((maxsize) * sizeof(short int));
 		}
 	}
-	
-	printf("\nCube created");
-
+*/	
+	//printf("\nCube created");
+	printf("\nVersion2\n");
 	/* Zero the cube */
 
 	for(i = 0; i < maxsize; i++) {
@@ -44,7 +45,7 @@ int main (int argc, char *argv[]) {
 		}
 	}
 
-	printf("\nCube zeroed");
+	//printf("\nCube zeroed");
 
 	double diffusion_coefficient = 0.175;
 	double room_dimension = 5;
@@ -56,7 +57,7 @@ int main (int argc, char *argv[]) {
 	// Initialize the fist cell
 	
 	cube[1][1][1] = 1.0e21;
-
+	/*
 	for(i = 0; i < maxsize; i++){
 		for(j = 0; j < maxsize; j++){
 			for(k = 0; k < maxsize; k++){
@@ -64,15 +65,20 @@ int main (int argc, char *argv[]) {
 			}
 		}
 	}
+	*/
 
-
-
+	maskX[0] = maskY[0] = maskZ[0] = 0;
+	maskX[maxsize-1] = maskY[maxsize-1] = maskZ[maxsize-1] = 0;
 	for (i=1;i<maxsize-1;i++){
+		maskX[i] = maskY[i] = maskZ[i] = 1;
+		
+		/*
 		for (j=1;j<maxsize-1;j++){
 			for (k=1;k<maxsize-1;k++){
 				mask[i][j][k] = 1;
 			}
 		}
+		*/
 	}
 
 	int pass = 0;
@@ -80,6 +86,7 @@ int main (int argc, char *argv[]) {
 	double ratio;
 	double change;
         double maxval, minval, value;
+	double sum = 0.0;
 
 	do{
 
@@ -88,32 +95,32 @@ int main (int argc, char *argv[]) {
 		for (i=1; i<maxsize-1; i++) { 
 			for (j=1; j<maxsize-1; j++) { 
 				for (k=1; k<maxsize-1; k++) { 
-					change = (cube[i][j][k]-cube[i+1][j][k])*DTerm*mask[i+1][j][k];
+					change = (cube[i][j][k]-cube[i+1][j][k])*DTerm*(maskX[i+1]*maskY[j]*maskZ[k]);
 					cube[i][j][k]-=change;
 					cube[i+1][j][k]+=change;
 
 
-					change = (cube[i][j][k]-cube[i-1][j][k])*DTerm*mask[i-1][j][k];
+					change = (cube[i][j][k]-cube[i-1][j][k])*DTerm*(maskX[i-1]*maskY[j]*maskZ[k]);
 					cube[i][j][k]-=change;
 					cube[i-1][j][k]+=change;
 
 
-					change = (cube[i][j][k]-cube[i][j+1][k])*DTerm*mask[i][j+1][k];
+					change = (cube[i][j][k]-cube[i][j+1][k])*DTerm*(maskX[i]*maskY[j+1]*maskZ[k]);
 					cube[i][j][k]-=change;
 					cube[i][j+1][k]+=change;
 
 
-					change = (cube[i][j][k]-cube[i][j-1][k])*DTerm*mask[i][j-1][k];
+					change = (cube[i][j][k]-cube[i][j-1][k])*DTerm*(maskX[i]*maskY[j-1]*maskZ[k]);
 					cube[i][j][k]-=change;
 					cube[i][j-1][k]+=change;
 	
 
-					change = (cube[i][j][k]-cube[i][j][k+1])*DTerm*mask[i][j][k+1];
+					change = (cube[i][j][k]-cube[i][j][k+1])*DTerm*(maskX[i]*maskY[j]*maskZ[k+1]);
 					cube[i][j][k]-=change;
 					cube[i][j][k+1]+=change;
 
 
-					change = (cube[i][j][k]-cube[i][j][k-1])*DTerm*mask[i][j][k-1];
+					change = (cube[i][j][k]-cube[i][j][k-1])*DTerm*(maskX[i]*maskY[j]*maskZ[k-1]);
 					cube[i][j][k]-=change;
 					cube[i][j][k-1]+=change;
 				}
@@ -142,7 +149,16 @@ int main (int argc, char *argv[]) {
 
 	} while(ratio < 0.99);
 
-	printf("\n%E ---- %E ---- %E\n", cube[1][1][1], cube[maxsize/2][maxsize/2][maxsize/2], cube[maxsize-2][maxsize-2][maxsize-2]);
+	for(i = 1; i < maxsize - 1; i++){
+		for(j = 1; j < maxsize - 1; j++){
+			for(k = 1; k < maxsize - 1; k++){
+				sum += cube[i][j][k];
+			}
+		}
+	}
+	printf("\nFinal # of particles: %E\n", sum);
+		
+	//printf("\n%E ---- %E ---- %E\n", cube[1][1][1], cube[maxsize/2][maxsize/2][maxsize/2], cube[maxsize-2][maxsize-2][maxsize-2]);
 
 	printf("\nBox equilibrated in %lf seconds of simulated time.\n", time);
 }
